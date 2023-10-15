b9c13fcc60418e68c6e8ba450acb20ea8e26f4ec
NISSAN-180SX-NISMO <semenaksin13@gmail.com>
2023-10-15 20:38:10 +0300

    ctrl alt l

--- Source/main.cpp
+++ Source/main.cpp
@@ -19,8 +19,8 @@ int main() {
     mainMenu.push_case("[3] = Изменить данные о студенте", KEY::THREE,StudentsManager::editStudent);
     mainMenu.push_case("[4] = Поиск студента...",          KEY::FOUR,findStudentMenu);
     findStudentMenu->push_case("[1] = Искать по ФИО", KEY::ONE, findStudentMenuByName);
-    findStudentMenuByName->push_case("[1] = Поиск с полным совпадением", KEY::ONE, [](){StudentsManager::findStudentByFullname(1);});
-    findStudentMenuByName->push_case("[2] = Поиск с неполным совпадением", KEY::TWO, [](){StudentsManager::findStudentByFullname(0.57);});
+    findStudentMenuByName->push_case("[1] = Поиск с полным совпадением",    KEY::ONE, [](){StudentsManager::findStudentByFullname(1);});
+    findStudentMenuByName->push_case("[2] = Поиск с неполным совпадением",  KEY::TWO, [](){StudentsManager::findStudentByFullname(0.57);});
     findStudentMenu->push_case("[2] = Искать по номеру группы", KEY::TWO, StudentsManager::findStudentByGroupNumber);
     findStudentMenu->push_case("[3] = Искать по среднему баллу...",  KEY::THREE,    findStudentByIdCardMenu);
     findStudentByIdCardMenu->push_case("[1] = Поиск по заданному значению", KEY::ONE, StudentsManager::findStudentByIdCard_Match);
